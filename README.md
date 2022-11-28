d1e04d53028ded3944dee8338ad8ce83b2726d4d
php <php__@tutamail.com>
2022-11-28 15:33:41 -0300

    Adiciona movimento do player

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 O Projeto Topazio é um jogo Shump (Shoot 'em up) inspirado na famosa série de jogos Touhou project feio em python.
 
+
+
+To do list:
+Fazer o player mover de para cima e para baixo
+Adicionar um modelo para o player
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-# Pygame template
+# Shmup game
 import pygame
 import random
 
-WIDTH = 360
-HEIGHT = 480
+WIDTH = 480
+HEIGHT = 600
 FPS = 60
 
 # Cores
@@ -20,6 +20,34 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Projeto Topásio")
 clock = pygame.time.Clock()
 
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 40))
+        self.image.fill(VERDE)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+
+    def update(self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -5
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 5
+        self.rect.x += self.speedx
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 # Game loop
 running = True
 while running:
@@ -32,9 +60,11 @@ while running:
             running = False
 
     # Update
+    all_sprites.update()
 
     # Render
     screen.fill(PRETO)
+    all_sprites.draw(screen)
     # Depois de rederizar tudo, "virar"/mostrar as cartas (os objetos)
     pygame.display.flip()
 
